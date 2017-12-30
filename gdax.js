6faff47631583064cf8cd2efd5f816d4918eadf2
Omar Aflak <aflakomar@hotmail.fr>
2017-12-30 19:09:43 +0100

    first commit

---
+++ gdax.js
@@ -0,0 +1,34 @@
+/**
+    DOCUMENTATION : https://github.com/coinbase/gdax-node
+**/
+const fs = require("fs");
+const config = JSON.parse(fs.readFileSync("account.key"));
+
+const Gdax = require('gdax');
+const key = config.gdax.key;
+const secret = config.gdax.secret;
+const passphrase = config.gdax.passphrase;
+const apiURI = 'https://api.gdax.com';
+const sandboxURI = 'https://api-public.sandbox.gdax.com';
+const authedClient = new Gdax.AuthenticatedClient(key, secret, passphrase, apiURI);
+const publicClient = new Gdax.PublicClient();
+
+publicClient.getProductTicker('ETH-EUR', function(error, response, data){
+    if(!error){
+        price = parseFloat(data.price);
+        authedClient.getAccounts(function(error, response, data){
+            if(!error){
+                for(var item of data){
+                    if(item.currency=="ETH"){
+                        units = item.balance;
+                        break;
+                    }
+                }
+
+                console.log("1 ETH = "+price+" EUR");
+                console.log("BALANCE = "+units+" ETH");
+                console.log("TOTAL = "+units*price+" EUR");
+            }
+        });
+    }
+});
